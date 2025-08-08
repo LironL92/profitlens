ce85dda3180f9fbfa7edc8654adbce3e4163e83c
LironL92 <liron@leiboff.me>
2025-08-08 12:24:35 +0200

    Add custom favicon files for browser, iPhone, and Android

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/logo.svg" />
+        <link rel="icon" href="/browser.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/iPhone.png" />
+        <link rel="icon" href="/Android.png" type="image/png" sizes="192x192" />
         <meta name="theme-color" content="#ec4899" />
       </head>
       <body className="antialiased">{children}</body>
